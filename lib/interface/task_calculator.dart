3cfa232a4a02100a204a76d59461c696336de016
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-30 05:54:17 +0100

    refactored the description of an interface

--- lib/interface/task_calculator.dart
+++ lib/interface/task_calculator.dart
@@ -1,5 +1,7 @@
 /* An interface is a contract that specifies or describes
-   the capabilities that a class should provide. */
+   the capabilities that a class should provide. 
+   
+   This is called programming to an interface -> */
 
 abstract class TaxCalculator {
   double calculateTax();
