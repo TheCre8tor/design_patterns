a5d42dad2fe3f57f215cd1a5855b4c2adf93e678
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-30 06:06:34 +0100

    Releared about how an interface works

--- bin/design_patterns.dart
+++ bin/design_patterns.dart
@@ -1,9 +1,18 @@
 import 'package:design_patterns/design_patterns.dart';
+import 'package:design_patterns/interface/task_calculator.dart';
+import 'package:design_patterns/interface/tax_calculator_2019.dart';
+import 'package:design_patterns/interface/tax_calculator_2020.dart';
 import 'package:design_patterns/oop_principles/abstraction.dart';
 import 'package:design_patterns/oop_principles/inheritance.dart';
 import 'package:design_patterns/oop_principles/polymorphism.dart';
 
 void main(List<String> arguments) {
+  // 0. Interface ->
+  final tax2019 = calculator(TaxCalculator2019());
+  final tax2020 = calculator(TaxCalculator2020());
+
+  print("Tax 2019 = $tax2019, and Tax 2020 = $tax2020");
+
   // 1. Encapsulation ->
   var account = Account();
   account.deposit(10);
@@ -35,4 +44,8 @@ void main(List<String> arguments) {
 
 void drawUIControl(UIWidget control) {
   control.draw();
+}
+
+double calculator(TaxCalculator tax) {
+  return tax.calculateTax();
 }
